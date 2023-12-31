2b982bcfe52cc91061ab3eb660c696edaf29cc4b
Replit user <>
2024-01-01 00:06:52 +0530

    added register controller

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import { app } from "./app.js"
 
 dotenv.config({ path: "./env" });
 
 connectDB()
   .then(() => {
+    app.on("error", (err) => {
+      console.log("Error :", err);
+      throw err;
+    });
+
     const PORT = process.env.PORT || 8000;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
